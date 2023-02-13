d385047fe1171ac4e9e6482203fac72acdde119b
Anas Amaneellah <anas@192.168.0.105>
2023-02-13 14:41:35 +0100

    added avatar to user

--- base/views.py
+++ base/views.py
@@ -4,9 +4,8 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message, User
-from .forms import RoomForm, UserForm
+from .forms import RoomForm, UserForm, MyUserCreationForm
 
 
 
@@ -45,10 +44,10 @@ def logoutUser(request):
 
 def registerPage(request):
 
-    form = UserCreationForm()
+    form = MyUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
 
         if form.is_valid():
             user = form.save(commit=False)
@@ -181,7 +180,7 @@ def updateUser(request):
     form = UserForm(instance=user)
 
     if request.method == 'POST':
-        form = UserForm(request.POST, instance=user)
+        form = UserForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
             form.save()
             return redirect('user-profile', pk=user.id)
